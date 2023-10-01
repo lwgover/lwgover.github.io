a250fd8f18d6f4e3c9546c318fec10300d53d367
lwgover <lgover@pugetsound.edu>
2023-10-01 11:24:24 -0500

    completed book recommendations service frontend

--- src/lib/Sections/Recommendations.svelte
+++ src/lib/Sections/Recommendations.svelte
@@ -1,86 +1,141 @@
 <script>
+	import ViewRecommendations from '$lib/Sections/view_recommendations.svelte';
+	var isSubmitted = false;
+	var output_data = { status: 'waiting', message: 'Submitting ...' };
 	function submitForm() {
 		// @ts-ignore
 		var bookname = document.getElementById('bookname');
 		// @ts-ignore
-		var author = document.getElementById('author');
+		var bookAuthor = document.getElementById('author');
+		// @ts-ignore
+		var bookRecommenderName = document.getElementById('recommender-name');
 		// @ts-ignore
 		var description = document.getElementById('description').value;
 		// @ts-ignore
 		if (bookname.value === '') {
 			// @ts-ignore
 			bookname.parentElement.style.color = 'red';
-            // @ts-ignore
-            bookname.style.borderColor = 'red';
 			// @ts-ignore
-        }else {
+			bookname.style.borderColor = 'red';
+			// @ts-ignore
+		} else {
 			// @ts-ignore
 			bookname.parentElement.style.color = '#555';
-            // @ts-ignore
-            bookname.style.borderColor = '#777';
-
+			// @ts-ignore
+			bookname.style.borderColor = '#777';
 		}
 		// @ts-ignore
-		if (author.value === '') {
+		if (bookAuthor.value === '') {
 			// @ts-ignore
-			author.parentElement.style.color = 'red';
-            // @ts-ignore
-            author.style.borderColor = 'red';
-		}else {
+			bookAuthor.parentElement.style.color = 'red';
 			// @ts-ignore
-			author.parentElement.style.color = '#555';
-            // @ts-ignore
-            author.style.borderColor = '#777';
-
+			bookAuthor.style.borderColor = 'red';
+		} else {
+			// @ts-ignore
+			bookAuthor.parentElement.style.color = '#555';
+			// @ts-ignore
+			bookAuthor.style.borderColor = '#777';
 		}
-        // @ts-ignore
-        if (!(bookname.value === '') && !(author.value === '')){
-			alert(
-				'Your book recommendation "' +
-					// @ts-ignore
-					bookname.value +
-					'" by ' +
-					// @ts-ignore
-					author.value +
-					' has been submitted! Desciption: ' +
-					description
-			);
+		// @ts-ignore
+		if (!(bookname.value === '') && !(bookAuthor.value === '')) {
+			const data = {
+				// @ts-ignore
+				book_name: bookname.value,
+				// @ts-ignore
+				author: author.value,
+				// @ts-ignore
+				recommender_name: bookRecommenderName.value,
+				recommendation: description
+			};
+			isSubmitted = true; // temp
+			console.log(JSON.stringify(data));
+			fetch('https://shhboimzz4.execute-api.us-east-1.amazonaws.com/BookAPI', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*'
+				},
+				body: JSON.stringify(data)
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					console.log('Success', data);
+					isSubmitted = true;
+					output_data = data;
+				});
 		}
 	}
 </script>
 
 <section class="book-recommendations">
 	<div class="book-recommendation-container">
-		<div class="recommendations-title">Recommend me a Book!</div>
+		{#if isSubmitted}
+			<div class="submission-container">
+				<div class="recommendations-title">Recommend me a Book!</div>
 
-		<form>
-			<label class="text-input-container">
-				Book Title
-				<input class="text-input" type="text" id="bookname" placeholder="Title" />
-			</label>
-			<label class="text-input-container">
-				Book Author
-				<input class="text-input" type="text" id="author" placeholder="Author" />
-			</label>
-			<label class="text-input-container">
-				Your Name (Optional)
-				<input class="text-input" type="text" id="author" placeholder="Your Name" />
-			</label>
-			<label class="text-input-container">
-				Description
-				<textarea
-					class="text-input description"
-					id="description"
-					placeholder="What makes it so good?"
-				/>
-			</label>
+				{#if output_data.status.localeCompare('waiting') == 0}
+				<div class="submission-subtitle">{JSON.stringify(output_data.message).slice(1, -1)}</div>
+					<div class="lds-dual-ring" />
+				{:else if output_data.status.localeCompare('succeeded') == 0}
+					<div class='submission-mid-title'>Thanks for your recommendation!</div>
+					<div class='submission-subtitle'>Check out some of the other reviews below</div>
+					<ViewRecommendations/>
+				{:else}
+					<div style='margin-top:30%'/>
+					<div class='submission-mid-title'>Unfortunately, the submission failed.</div>
+					<input class="submit-button" type="submit" value="Try Again?" on:click={function() {isSubmitted = false; output_data = { status: 'waiting', message: 'Submitting ...' };}} />
+				{/if}
+			</div>
+		{:else}
+			<div class="recommendations-title">Recommend me a Book!</div>
 
-			<input class="submit-button" type="submit" value="Submit →" on:click={() => submitForm()} />
-		</form>
+			<form>
+				<label class="text-input-container">
+					Book Title
+					<input class="text-input" type="text" id="bookname" placeholder="Title" />
+				</label>
+				<label class="text-input-container">
+					Book Author
+					<input class="text-input" type="text" id="author" placeholder="Author" />
+				</label>
+				<label class="text-input-container">
+					Your Name (Optional)
+					<input class="text-input" type="text" id="recommender-name" placeholder="Your Name" />
+				</label>
+				<label class="text-input-container">
+					Description
+					<textarea
+						class="text-input description"
+						id="description"
+						placeholder="What makes it so good?"
+					/>
+				</label>
+
+				<input class="submit-button" type="submit" value="Submit →" on:click={() => submitForm()} />
+			</form>
+		{/if}
 	</div>
 </section>
 
 <style>
+	.submission-mid-title {
+		margin: auto;
+		text-align: center;
+		font-size: 3vh;
+		padding-left: 5vw;
+		padding-right: 5vw;
+		margin:2vh;
+	}
+	.submitted-container {
+		width: 100%;
+	}
+	.submission-subtitle {
+		margin: auto;
+		text-align: center;
+		font-size: 2.5vh;
+		padding-left: 5vw;
+		padding-right: 5vw;
+	}
 	.text-input-container {
 		margin-left: 10%;
 		margin-right: 10%;
@@ -134,7 +189,7 @@
 	}
 	.book-recommendation-container {
 		background-color: white;
-		width: 500px;
+		width: 600px;
 		height: 92vh;
 		margin: auto;
 		margin-top: 4vh;
@@ -153,9 +208,37 @@
 		text-align: center;
 		font-weight: 500;
 	}
-	@media screen and (max-width: 550px) {
+	@media screen and (max-width: 650px) {
 		.book-recommendation-container {
-			width:60vh;
+			width: 50vh;
+		}
+	}
+
+	/** From https://loading.io/css/*/
+	.lds-dual-ring {
+		width: 11vh;
+		height: 12vh;
+		margin:auto;
+		margin-top: 30%;
+		translate: -2vh 0;
+	}
+	.lds-dual-ring:after {
+		content: ' ';
+		display: block;
+		width: 10vh;
+		height: 10vh;
+		margin: 8px;
+		border-radius: 50%;
+		border: 1vh solid #767676;
+		border-color: #767676 transparent #fff transparent;
+		animation: lds-dual-ring 1.2s linear infinite;
+	}
+	@keyframes lds-dual-ring {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
 		}
 	}
 </style>
---
+++ src/lib/Sections/view_recommendations.svelte
@@ -0,0 +1,165 @@
+<script>
+	// @ts-nocheck
+
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+	// @ts-nocheck
+	let reviews = [
+		{ loading: 'true' },
+		{ loading: 'true' },
+		{ loading: 'true' },
+		{ loading: 'true' },
+		{ loading: 'true' },
+		{ loading: 'true' },
+		{ loading: 'true' },
+		{ loading: 'true' },
+		{ loading: 'true' },
+		{ loading: 'true' },
+		{ loading: 'true' },
+		{ loading: 'true' }
+	];
+	function load_reviews() {
+		fetch('https://shhboimzz4.execute-api.us-east-1.amazonaws.com/BookAPI', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			}
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log('Success', data);
+
+				let loaded_reviews = JSON.parse(data['message']);
+				console.log(loaded_reviews);
+				for (var i = 0; i < 12; i++) {
+					reviews[i] = loaded_reviews[i];
+				}
+			});
+	}
+	onMount(() => load_reviews());
+</script>
+
+<div class="view-recommendations-container">
+	{#each reviews as review, i}
+		<div class="review-card-container">
+			{#if review['loading']}
+				<div
+					class="review-card-loading-title review-card-loading"
+					transition:fade={{ delay: 100 + 300 * i, duration: 300 }}
+				>
+					<div class="loading-shine" />
+				</div>
+			{:else}
+				<div class="review-card-title" transition:fade={{ delay: 650 + 300 * i, duration: 300 }}>
+					{review['book_name']}
+				</div>
+			{/if}
+			{#if review['loading']}
+				<div
+					class="review-card-loading-author review-card-loading"
+					transition:fade={{ delay: 200 + 300 * i, duration: 300 }}
+				>
+					<div class="loading-shine" />
+				</div>
+			{:else}
+				<div class="review-card-author" transition:fade={{ delay: 650 + 300 * i, duration: 300 }}>
+					{review['author']}
+				</div>
+			{/if}
+			{#if review['loading']}
+				<div
+					class="review-card-loading-desc review-card-loading"
+					transition:fade={{ delay: 300 + 300 * i, duration: 300 }}
+				>
+					<div class="loading-shine" />
+				</div>
+			{:else}
+				<div class="review-card-desc" transition:fade={{ delay: 650 + 300 * i, duration: 300 }}>
+					{review['description']}
+				</div>
+			{/if}
+		</div>
+	{/each}
+</div>
+
+<style>
+	.review-card-desc {
+		width: 100%;
+		border-radius: 5px;
+		margin-top: 15px;
+        color: #333;
+	}
+	.review-card-title {
+		width: 50%;
+		font-size: 3vh;
+		border-radius: 5px;
+        font-weight: 550;
+        color: #222;
+        letter-spacing: 2px;
+	}
+	.review-card-author {
+		width: 40%;
+		font-size: 2vh;
+		border-radius: 5px;
+		margin-top: 5px;
+        color: #666;
+        font-style: italic;
+	}
+	.review-card-loading-desc {
+		width: 100%;
+		height: 5vh;
+		border-radius: 5px;
+		margin-top: 15px;
+	}
+	.review-card-loading-title {
+		width: 50%;
+		height: 3vh;
+		border-radius: 5px;
+	}
+	.review-card-loading-author {
+		width: 40%;
+		height: 2vh;
+		border-radius: 5px;
+		margin-top: 5px;
+	}
+	.review-card-loading {
+		background-color: #ddd;
+		overflow: hidden;
+	}
+	.loading-shine {
+		position: relative;
+		top: 0;
+		height: 100%;
+		width: 6vh;
+		background: linear-gradient(to right, transparent, rgba(255, 255, 255, 0.7), transparent);
+		animation: shine 2.5s infinite linear;
+		transform: translate(-100px, 0px);
+	}
+	@keyframes shine {
+		0% {
+			transform: translate(-150px, 0px);
+		}
+		100% {
+			transform: translate(500px, 0px);
+		}
+	}
+	.review-card-container {
+		width: 100%;
+		background-color: white;
+		margin-bottom: 20px;
+		border-radius: 1.3vh;
+		padding: 20px;
+		box-shadow: #bcbcbc 0 0 10px 5px;
+		overflow: hidden;
+	}
+	.view-recommendations-container {
+		height: 55vh;
+		overflow-x: hidden;
+		background-color: #ddd;
+		margin: 20px;
+		border-radius: 2vh;
+		padding: 13px;
+		overflow-y: auto;
+	}
+</style>
